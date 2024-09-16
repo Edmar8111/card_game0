476cae4485b6830377811387b3e548bd767bbbf9
Edmar D. O. Filho <87383421+Edmar8111@users.noreply.github.com>
2024-09-16 16:25:32 -0400

    Delete cards_game_0 directory

--- cards_game_0/home_0/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Cards_db
-
-# Register your models here.
-admin.site.register(Cards_db)
--- cards_game_0/home_0/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Cards_db(models.Model):
-    name=models.CharField(max_length=50, default='None')
-    image_card=models.ImageField(upload_to='image_cards')
-    overhead = models.DecimalField(decimal_places=1, default=0, max_digits=5) 
-    verso_card_img = models.ImageField(upload_to='image_cards')
-    
