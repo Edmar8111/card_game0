f8776797eaa50caf36de8eb5931bade1f12330bb
Edmar D. O. Filho <87383421+Edmar8111@users.noreply.github.com>
2024-09-17 16:18:11 -0400

    Delete cards_game_0 directory

--- cards_game_0/home_0/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Cards_db,Images
-
-# Register your models here.
-admin.site.register(Cards_db)
-admin.site.register(Images)
--- cards_game_0/home_0/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Home0Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'home_0'
